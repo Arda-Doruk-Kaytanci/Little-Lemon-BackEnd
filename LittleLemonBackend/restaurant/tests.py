111a6ca7be569d9e4c9731a5bd2ee75e001348e0
Arda-Doruk-Kaytanci <ardadorukkaytanci@gmail.com>
2024-08-17 18:01:57 +0300

    awdwadwadwa

--- LittleLemonBackend/restaurant/tests.py
+++ LittleLemonBackend/restaurant/tests.py
@@ -1,41 +1,29 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
 
 
-class ReservationsViewTestCaseUnAuth(TestCase):
-
-    def setUp(self):    
-        self.user = User.objects.create_user(username="testuser", password="testpass")
-
-    def test_reservations_view_redirects_unauthenticated_user(self):
-        response = self.client.get(reverse("reservations"))
-        self.assertEqual(response.status_code, HttpResponseRedirect.status_code)
-        self.assertRedirects(
-            response, f'/accounts/login/?next={reverse("reservations")}'
-        )
-
-
-class ReservationsViewTestCaseNonSuper(TestCase):
+class ReservationsViewTestCase(TestCase):
 
     def setUp(self):
+        # Create test user and superuser
         self.user = User.objects.create_user(username="testuser", password="testpass")
-
-    def test_reservations_view_denied_for_non_superuser(self):
-        self.client.login(username="testuser", password="testpass")
-        response = self.client.get(reverse("reservations"))
-        self.assertEqual(response.status_code, 403)
-
-
-class ReservationsViewTestCaseForSuper(TestCase):
-
-    def setUp(self):
         self.superuser = User.objects.create_superuser(
             username="superuser", password="superpass"
         )
 
+    def test_reservations_view_redirects_unauthenticated_user(self):
+        response = self.client.get(reverse("reservations"))
+        self.assertEqual(response.status_code, 403)
+
+    def test_reservations_view_denied_for_non_superuser(self):
+        # Test that non-superusers receive a 403 Forbidden response
+        self.client.login(username="testuser", password="testpass")
+        response = self.client.get(reverse("reservations"))
+        self.assertEqual(response.status_code, 403)  # Check for Forbidden response
+
     def test_reservations_view_accessible_for_superuser(self):
+        # Test that superusers can access the view
         self.client.login(username="superuser", password="superpass")
         response = self.client.get(reverse("reservations"))
-        self.assertEqual(response.status_code, 200)  # OK
+        self.assertEqual(response.status_code, 200)  # Check for OK response
